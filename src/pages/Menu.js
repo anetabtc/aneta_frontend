b4fb8600d0cc3710b66b7d553b5d09ea2166ce0e
kozhamseitova <kozhamseitova91@gmail.com>
2022-08-16 14:38:39 +0600

    menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -6,15 +6,52 @@ function Menu() {
         <div className="sidebar">
             <div className="sidebar_content">
                 <ul className="menuList">
-            <Link to="/"><li className="btnM">Bridge</li></Link>
-            <Link to="/transactions"><li className="btnM">Transactions</li></Link>
-            <Link to="/dashboard"><li className="btnM">Dashboard</li></Link>
-            <Link to="/vault"><li className="btnM">Vault</li></Link>
+            <Link to="/" className="menu-item">
+                <div className="menu-icon">
+                    <img src={require('./img/bridge.png')} id="Vector" />
+                </div>
+                <li className="btnM">
+                    Bridge
+                </li></Link>
+            <Link to="/transactions" className="menu-item">
+                <div className="menu-icon">
+                    <img src={require('./img/transactions.png' )} id="Vector"/>
+                </div>
+                <li className="btnM">
+                    Transactions
+                </li></Link>
+            <Link to="/dashboard" className="menu-item">
+                <div className="menu-icon">
+                    <img src={require('./img/dashboard.png')} id="Vector"/>
+                </div>
+                <li className="btnM">
+                    Dashboard
+                </li></Link>
+            <Link to="/vault" className="menu-item">
+                <div className="menu-icon">
+                    <img src={require('./img/vault.png')} id="Vector"/>
+                </div>
+                <li className="btnM">
+                    Vault
+                </li></Link>
             </ul>
+                <br/>
             <hr id="menuHR"></hr>
             <ul>
-            <Link to="/feedback"><li className="btnM">Feedback</li></Link>
-            <Link to="/docs"><li className="btnM">Docs</li></Link>
+            <Link to="/feedback" className="menu-item">
+                <div className="menu-icon">
+                    <img src={require('./img/feedback.png')} id="Vector"/>
+                </div>
+                <li className="btnM">
+                    Feedback
+                </li></Link>
+            <Link to="/docs" className="menu-item">
+                <div className="menu-icon">
+                    <img src={require('./img/docs.png')} id="Vector"/>
+                </div>
+                <li className="btnM">
+                    Docs
+                </li></Link>
             </ul>
             </div>
         </div>
