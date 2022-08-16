a35c1bda0d5bb6ee0e33ce93487ebd9f0c4321df
kozhamseitova <kozhamseitova91@gmail.com>
2022-08-16 19:44:03 +0600

    navbar

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,21 +1,66 @@
-import react from "react";
-import {Link} from "react-router-dom"
+import react, {useState} from "react";
+
 
 function Navbar() {
+    const[selected, setSelected] = useState("Select Network")
     return (
-        <div id="navbar_menu"> 
+        <div id="navbar_menu">
         <div id="imgLogonav">
            <img src={require('./img/logo.png')} className="imgLogo" />
            </div>
            <div id="head">
-           <button type="button" id="notConnect_wallet">Connect Wallet</button>
-        <button type="button" id="menuButton">Wallet(backend)</button>
-        <button type="button" id="menuButton">Address</button>
-        <button type="button" id="menuButton_sun"><img src={require('./img/Vector.png')} id="Vector" /></button>
+               <div><DropDown selected={selected} setSelected={setSelected}/></div>
+               <div><button type="button" className="menuButton">Address</button></div>
+               <div><button type="button" className="menuButton"><img src={require('./img/Vector.png')} id="Vector" /></button></div>
            </div>
         </div>
 
 
     )
 }
+
+
+function DropDown({selected, setSelected}) {
+    const [isActive, setIsActive] = useState(false)
+
+    function DownUp(){
+        if(isActive){
+            <img src={require('./img/up.png')}/>
+        }else{
+            <img src={require('./img/down.png')}/>
+        }
+    }
+    const options = ["Ergo", "Cardano"];
+    return (
+        <div className="dropdown">
+            <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
+                {selected != "Select Network" && (
+                    <img src={require('./img/' + selected + '.png')} id="Vector" />
+                )}
+                <p>{selected}</p>
+                <img src={require('./img/down.png')} id="down"/>
+
+            </div>
+
+
+
+            {isActive && (
+                <div className="dropdown-content">
+                    {options.map((option) => (
+                        <div onClick={(e) => {
+                            setSelected(option)
+                            setIsActive(false)
+                        }}
+                             className="dropdown-item" >
+                        <img src={require('./img/' + option + '.png')} id="Vector" />
+                            {option}
+                        </div>
+                        ))}
+                </div>
+            )}
+        </div>
+    )
+}
+
+
 export default Navbar;
