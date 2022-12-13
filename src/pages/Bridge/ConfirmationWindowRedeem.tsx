8f52ee79cc9a1f51bcabb9b5a1dd492b80b7e36a
Willie McClinton <wbm3.important@gmail.com>
2022-12-13 01:27:38 -0500

    added fb db write

--- src/pages/Bridge/ConfirmationWindowRedeem.tsx
+++ src/pages/Bridge/ConfirmationWindowRedeem.tsx
@@ -12,13 +12,6 @@ import { getAnalytics } from "firebase/analytics";
 // Add a second document with a generated ID.
 import { addDoc, collection, getDocs } from "firebase/firestore"; 
 const firebaseConfig = {
-    "apiKey": "AIzaSyDkTWwt7X5XKgB9Uh7qe98sUqYdcNgfyZ4",
-    "authDomain": "anetabtc-dcd3c.firebaseapp.com",
-    "projectId": "anetabtc-dcd3c",
-    "storageBucket": "anetabtc-dcd3c.appspot.com",
-    "messagingSenderId": "834678075786",
-    "appId": "1:834678075786:web:650ae8da7ed1e1a32f1873",
-    "measurementId": "G-VQ2B0CHHC7"
 }
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
