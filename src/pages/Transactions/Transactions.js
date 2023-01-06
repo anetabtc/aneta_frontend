46ace5833ebffce5a769c0f07672438e8ec7a74e
darkhan-b <xxlabbesxx@gmail.com>
2023-01-06 17:46:16 +0800

    transcation update date and conditions

--- src/pages/Transactions/Transactions.js
+++ src/pages/Transactions/Transactions.js
@@ -37,8 +37,12 @@ function Transactions() {
 
         })
         console.log(txs)
+        console.log(txs.length)
+        // console.log(txs.info)
 
-        setProducts(txs)
+                setProducts(txs)
+
+
 
 
 
@@ -61,25 +65,45 @@ function Transactions() {
                         </tr>
 
                         {products.map((tx) => (
+                            tx.info == "Mint Order Submitted" || tx.info == "Mint Order Paid" ?
                             <tr key={tx.id}>
-                                <td>{tx.datetime}</td>
-                                <td>{tx.amount}</td>
-                                <td>{tx.amount}</td>
-                                <td>{tx.erg_txid}</td>
-                                <td>{tx.erg_txid}</td>
+
+                                <td>{new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(tx.datetime)}</td>
+                                <td>{tx.amount} BTC</td>
+                                <td>{tx.amount} eBTC</td>
+                                <td class="underline">{tx.btc_address}</td>
+                                <td class="underline">{tx.erg_address}</td>
                                 <td>{tx.info}</td>
+
                             </tr>
+                                : null
                         ))}
                     </table>
                     <br/>
                     <p class="transactionTitle2">Unwrap Requests</p>
                     <table>
                         <tr>
-                            <td>Updated</td>
-                            <td>Amount (anetaBTC)</td>
+                            <td>Created at</td>
+                            <td>Amount (BTC)</td>
+                            <td>Amount (eBTC)</td>
                             <td>BTC Transaction</td>
+                            <td>ERG transaction</td>
                             <td>Confirmation Status</td>
                         </tr>
+                        {products.map((tx) => (
+                            tx.info == "Redeem Order Submitted" ?
+                                <tr key={tx.id}>
+
+                                    <td>{new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(tx.datetime)}</td>
+                                    <td>{tx.amount} BTC</td>
+                                    <td>{tx.amount} eBTC</td>
+                                    <td class="underline">{tx.btc_address}</td>
+                                    <td class="underline">{tx.erg_address}</td>
+                                    <td>{tx.info}</td>
+
+                                </tr>
+                                : null
+                        ))}
                     </table>
                 </b>
             </div>
