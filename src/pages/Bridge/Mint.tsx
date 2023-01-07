df9bb832bae981a947a2e3fe08322e1e38cef07f
mnamadina <madina@brandeis.edu>
2023-01-06 19:00:16 -0500

    I deleted stuff

--- src/pages/Bridge/Mint.tsx
+++ src/pages/Bridge/Mint.tsx
@@ -5,7 +5,6 @@ import {useState} from "react";
 import {useEffect, useRef} from "react";
 import {formatData} from "./Utils";
 import BTCDeposit from "./BTCDeposit";
-// I changed something
 
 function Mint({eBTC, bridgeFee, nautilusaddress}) {
     const [address, setAddress] = useState('');
