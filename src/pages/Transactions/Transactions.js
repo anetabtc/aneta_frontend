aae888025d3c6493ed4492423285722d9c2e0bc7
darkhan-b <xxlabbesxx@gmail.com>
2023-01-08 16:16:05 +0800

    frontend trans

--- src/pages/Transactions/Transactions.js
+++ src/pages/Transactions/Transactions.js
@@ -8,6 +8,9 @@ import {getAnalytics} from "firebase/analytics";
 // Add a second document with a generated ID.
 import {addDoc, collection, getDocs, doc} from "firebase/firestore";
 import firebaseConfig from "../Bridge/firebaseConfig";
+import ConfirmationWindow from "../Bridge/ConfirmationWindow";
+import ConfirmationWindowRedeem from "../Bridge/ConfirmationWindowRedeem";
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -32,29 +35,97 @@ function Transactions() {
         snapshot.forEach(doc => {
             txs.push(
                 doc.data()
-
             )
 
         })
-        console.log(txs)
-        console.log(txs.length)
-        // console.log(txs.info)
-
-                setProducts(txs)
 
 
+        setProducts(txs)
+    }
+        const [visible, SetVisible] = useState(true);
 
+        function DownUp() {
+            if (visible) {
 
+                return (
+                    <MintPage/>
+                )
+            } else {
+                return (
+                    <RedeemPage/>
+                )
+            }
+        }
 
+        return (
 
+                <div>
+
+                    <div id="radiosTrans">
+                        <input id="rad3" type="radio" name="radioBtn" onClick={() => SetVisible(true)}/>
+                        <label className="labelsTr" htmlFor="rad3"><b>Wrap Requests</b></label>
+                        <input id="rad4" type="radio" name="radioBtn" onClick={() => SetVisible(false)}/>
+                        <label className="labelsTr" htmlFor="rad4"><b>Unwrap Request</b></label>
+                        <div id="bckgrnd1"></div>
+
+                    </div>
+                    <DownUp/>
+
+            </div>
+        )
+
+    function MintPage() {
+
+        return (
+            <div className='mainmenu_transaction'>
+
+                    <p className="transactionTitle1">Wrap Requests</p>
+
+                        <table className="tableWrap">
+                            <tr border="0">
+                                <td>Created at</td>
+                                <td>Transaction (BTC)</td>
+                                <td>Transaction (eBTC)</td>
+                                <td>Transaction (ERG)</td>
+                                <td>anetaBTC ID</td>
+                                <td>Confirmation Status</td>
+                            </tr>
+                            <hr className="menuHR"/>
+                            {products.map((tx) => (
+                                tx.info == "Mint Order Submitted" || tx.info == "Mint Order Paid" ?
+                                    <tr key={tx.id}>
+
+                                        <td>{new Intl.DateTimeFormat('en-US', {
+                                            year: 'numeric',
+                                            month: '2-digit',
+                                            day: '2-digit',
+                                            hour: '2-digit',
+                                            minute: '2-digit',
+                                            second: '2-digit'
+                                        }).format(tx.datetime)}</td>
+                                        <td>{tx.amount} BTC</td>
+                                        <td>{tx.amount} eBTC</td>
+                                        <td className="underline">{tx.btc_address}</td>
+                                        <td className="underline">{tx.erg_address}</td>
+                                        <td>{tx.info}</td>
+
+                                    </tr>
+                                    : null
+                            ))}
+                        </table>
+
+                </div>
+    )
     }
 
-    return (
-        <div className='mainmenu_transaction'>
-            <div className="menuTr">
-                <p class="transactionTitle1">Wrap Requests</p>
-                <b>
-                    <table>
+    function RedeemPage() {
+        return (
+            <div className='mainmenu_transaction'>
+                <div>
+
+
+                    <p class="transactionTitle1">Unwrap Requests</p>
+                    <table className="tableWrap">
                         <tr>
                             <td>Created at</td>
                             <td>Amount (BTC)</td>
@@ -63,38 +134,19 @@ function Transactions() {
                             <td>ERG transaction</td>
                             <td>Confirmation Status</td>
                         </tr>
-
-                        {products.map((tx) => (
-                            tx.info == "Mint Order Submitted" || tx.info == "Mint Order Paid" ?
-                            <tr key={tx.id}>
-
-                                <td>{new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(tx.datetime)}</td>
-                                <td>{tx.amount} BTC</td>
-                                <td>{tx.amount} eBTC</td>
-                                <td class="underline">{tx.btc_address}</td>
-                                <td class="underline">{tx.erg_address}</td>
-                                <td>{tx.info}</td>
-
-                            </tr>
-                                : null
-                        ))}
-                    </table>
-                    <br/>
-                    <p class="transactionTitle2">Unwrap Requests</p>
-                    <table>
-                        <tr>
-                            <td>Created at</td>
-                            <td>Amount (BTC)</td>
-                            <td>Amount (eBTC)</td>
-                            <td>BTC Transaction</td>
-                            <td>ERG transaction</td>
-                            <td>Confirmation Status</td>
-                        </tr>
+                        <hr className="menuHR"/>
                         {products.map((tx) => (
                             tx.info == "Redeem Order Submitted" ?
                                 <tr key={tx.id}>
 
-                                    <td>{new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(tx.datetime)}</td>
+                                    <td>{new Intl.DateTimeFormat('en-US', {
+                                        year: 'numeric',
+                                        month: '2-digit',
+                                        day: '2-digit',
+                                        hour: '2-digit',
+                                        minute: '2-digit',
+                                        second: '2-digit'
+                                    }).format(tx.datetime)}</td>
                                     <td>{tx.amount} BTC</td>
                                     <td>{tx.amount} eBTC</td>
                                     <td class="underline">{tx.btc_address}</td>
@@ -105,16 +157,12 @@ function Transactions() {
                                 : null
                         ))}
                     </table>
-                </b>
+
+                </div>
             </div>
-        </div>
-    )
+        )
+
+    }
 }
 
 export default Transactions;
-<script type="text/javascript">
-
-</script>
-
-
-
