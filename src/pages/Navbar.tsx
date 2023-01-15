f1e6e2bb1b0f44e87e98ad4c468eb30882098822
kozhamseitova <kozhamseitova91@gmail.com>
2023-01-15 19:54:52 +0600

    tx page

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import {SetStateAction, useState} from "react";
-import getAddress from "./Bridge/address";
+import React, {useEffect} from "react";
+import {useState} from "react";
 
 
 function Navbar() {
