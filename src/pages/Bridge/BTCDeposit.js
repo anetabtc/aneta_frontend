d444a73724916ac71d9f6e2f5bea72b11d72aa82
esis8 <eesis.dev@gmail.com>
2023-01-18 13:12:34 -0300

    menu deposit

--- src/pages/Bridge/BTCDeposit.js
+++ src/pages/Bridge/BTCDeposit.js
@@ -6,20 +6,27 @@ function BTCDeposit({eBTC, bridgeFee}) {
         window.location.reload();
     }
 
-    return(
+     return(
         <div className="mainPopup">
-            <div className="confContent">
+            <div className="confContent" id="top_card">
                 <div className="confWindow">
                     <div className="confTitle">
                         <div>
-                            {/*<img id="bit" src={require('../img/Bitcoin.png').default}*/}
-                            {/*      alt="aneta"/>*/}
+                        <img id="bitcoin" src={require('../img/Bitcoin.png').default} alt="aneta"/>
                             BTC Deposit</div>
                     </div>
                     <div className="confSubmission">
-                        <BTCDepositSuccess/>
+                    <div className="depositBTC">
+                        <div>Bridge Fee Payment Submitted</div>
+                        <div>eBTC will arrive in your wallet shortly.</div>
+                        <div>Transactions can take around 10 minutes to process.</div>
                         <CheckMark/>
+                        <div>Your unique anetaBTC ID for this entire transaction is:</div>
+                        <div id="idTransaction"><b>"Pendind idXXXXX"</b></div>
+                        <div>This unique ID is also available in your Transactions tab. If you need support, this ID will help us assist you.</div>
                         <button type="button" id="confButton1" className="confWRS" onClick={refreshPage}><b>Continue</b></button>
+                    </div>
+                        
                     </div>
                 </div>
             </div>
@@ -27,11 +34,6 @@ function BTCDeposit({eBTC, bridgeFee}) {
     )
 }
 
-function BTCDepositSuccess(){
-    return (
-        <div className="textBTC"></div>
-    )
-}
 
 export default BTCDeposit
 
