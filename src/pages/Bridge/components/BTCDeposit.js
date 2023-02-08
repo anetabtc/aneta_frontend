5088a2750bae76228d83f767297a3744b86457ab
esis8 <eesis.dev@gmail.com>
2023-02-08 20:59:46 -0300

    wrap

--- src/pages/Bridge/components/BTCDeposit.js
+++ src/pages/Bridge/components/BTCDeposit.js
@@ -25,7 +25,7 @@ function BTCDeposit({eBTC, bridgeFee, nautilusaddress}) {
 
      return(
         <div className="mainPopup">
-            <div className="confContent" id="top_card">
+{/*             <div className="confContent" id="top_card">
                 <div className="confWindow">
                     <div className="confTitle">
                         <div>
@@ -42,6 +42,33 @@ function BTCDeposit({eBTC, bridgeFee, nautilusaddress}) {
                     </div>
                         
                     </div>
+                </div>
+            </div> */}
+            <div className="confContent" id="top_card">
+                <div className="confWindow btcDeposit">
+                    <div className="titleBTC">BTC Deposit</div>
+                    <div className="resultBridge">
+                        <div className="amountBridge">
+                            <div className="card">
+                                <img id="bitcoin" src={require('../../../assets/img/Bitcoin.png').default} alt="btc"/>
+                                <p>BTC</p>
+                            </div>
+                            <div>{eBTC}</div>
+                        </div>
+                        <div className="arrowBridge"><img src={require('../../../assets/img/arrow_blue.png').default} alt="arrow"/></div>
+                        <div className="amountBridge">
+                            <div className="card">
+                                <img id="bitcoin" src={require('../../../assets/img/werg.png').default} alt="eBTC"/>
+                                <p>eBTC</p>
+                            </div>
+                            <div>{Math.round((parseFloat(eBTC)*.995)*100000000)/100000000}</div>
+                        </div>
+                    </div>
+                    <div className="text1">Thank you for sending your BTC Deposit.</div>
+                    <div className="text1">eBTC will be sent to your Cardano wallet once your BTC deposit is confirmed. This process may take up to 24 hours.</div>
+                    <div className="text2">The status and details of this transaction can be found in the “Transactions” tab on the side menu.</div>
+                    <div className="text2"><b><div>Support</div></b><br/>If you need support, your BTC transaction ID and your ERG transaction ID will help us assist you.</div>
+                    <button type="button" id="confButton1" className="confWRS" onClick={refreshPage}><b>Close</b></button>
                 </div>
             </div>
         </div>
